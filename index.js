262c3f07d60a50c119be8fbf875ec92c284065d6
Tyoma <risendrums777@gmail.com>
2021-10-20 22:18:18 +0300

    add

---
+++ index.js
@@ -0,0 +1,45 @@
+const vanillaOutput = document.querySelector('.output.vanilla');
+const throttledOutput = document.querySelector('.output.throttled');
+const trailingOutput = document.querySelector('.output.trailing');
+const leadingOutput = document.querySelector('.output.leading');
+const eventCounter = {
+  vanilla: 0,
+  throttled: 0,
+  trailing: 0,
+  leading: 0,
+};
+
+// Trailing debounce
+document.addEventListener(
+  'scroll',
+  _.debounce(() => {
+    eventCounter.trailing += 1;
+    trailingOutput.textContent = eventCounter.trailing;
+  }, 300),
+);
+
+// Leading debounce
+document.addEventListener(
+  'scroll',
+  _.debounce(
+    () => {
+      eventCounter.leading += 1;
+      leadingOutput.textContent = eventCounter.leading;
+    },
+    300,
+    { trailing: false, leading: true },
+  ),
+);
+
+document.addEventListener('scroll', () => {
+  eventCounter.vanilla += 1;
+  vanillaOutput.textContent = eventCounter.vanilla;
+});
+
+document.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    eventCounter.throttled += 1;
+    throttledOutput.textContent = eventCounter.throttled;
+  }, 300),
+);
